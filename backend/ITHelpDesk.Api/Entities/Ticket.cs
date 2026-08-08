5ccb0d07143536166adc68809aa1994e24207cf9
Hassan Hajj <hajjh480@gmail.com>
2026-08-08 18:35:28 +0300

    Add Week 5 notification backend foundation

--- backend/ITHelpDesk.Api/Entities/Ticket.cs
+++ backend/ITHelpDesk.Api/Entities/Ticket.cs
@@ -63,11 +63,14 @@ public class Ticket
         = new List<TicketHistory>();
 
     public ICollection<TicketComment> Comments { get; set; }
-    = new List<TicketComment>();
+        = new List<TicketComment>();
 
-public ICollection<TicketAttachment> Attachments { get; set; }
-    = new List<TicketAttachment>();
+    public ICollection<TicketAttachment> Attachments { get; set; }
+        = new List<TicketAttachment>();
 
-public ICollection<ActivityLog> ActivityLogs { get; set; }
-    = new List<ActivityLog>();
+    public ICollection<ActivityLog> ActivityLogs { get; set; }
+        = new List<ActivityLog>();
+
+    public ICollection<Notification> Notifications { get; set; }
+        = new List<Notification>();
 }
